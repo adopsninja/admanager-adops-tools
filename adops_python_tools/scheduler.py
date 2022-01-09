f424a69f9c5126bbf2adabd82426a2dcedbda46e
adopsninja <siudak.dariusz@gmail.com>
2022-01-09 20:17:08 +0100

    Added separate to schedule different tasks.

---
+++ adops_python_tools/scheduler.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from gam import AdOpsTools
+
+def main():
+    AdOpsTools().update_performance_placements("Company Y")
+    AdOpsTools().update_performance_placements("Company E")
+    AdOpsTools().update_performance_placements("Company I")
+    AdOpsTools().update_performance_placements("Company p")
+
+if __name__ == "__main__":
+    main()
