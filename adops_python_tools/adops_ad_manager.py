8b8eb86bebd5d9e90152e5b9c9be9eacce92297f
adopsninja <siudak.dariusz@gmail.com>
2022-03-22 23:20:05 +0100

    Use constant API_VERSION. Add statement building methods.

--- adops_python_tools/adops_ad_manager.py
+++ adops_python_tools/adops_ad_manager.py
@@ -2,6 +2,7 @@ import logging
 
 from googleads.ad_manager import AdManagerClient
 from googleads.oauth2 import GoogleRefreshTokenClient
+from googleads.ad_manager import StatementBuilder
 
 from constants import API_VERSION
 from database import Database
@@ -9,17 +10,15 @@ from database import Database
 logger = logging.getLogger(__name__)
 
 class AdOpsAdManagerClient:
-    _API_VERSION = API_VERSION
-
     def __init__(self, email, network_code=None) -> None:
         self.email = email
         self.client = self.set_admanager_client(network_code)
-        self.network_service = self.client.GetService("NetworkService", version=self._API_VERSION)
-        self.placement_service = self.client.GetService("PlacementService", version=self._API_VERSION)
-        self.inventory_service = self.client.GetService("InventoryService", version=self._API_VERSION)
-        self.site_service = self.client.GetService("SiteService", version=self._API_VERSION)
-        self.company_service = self.client.GetService("CompanyService", version=self._API_VERSION)
-        self.report_downloader = self.client.GetDataDownloader(version=self._API_VERSION)
+        self.network_service = self.client.GetService("NetworkService", version=API_VERSION)
+        self.placement_service = self.client.GetService("PlacementService", version=API_VERSION)
+        self.inventory_service = self.client.GetService("InventoryService", version=API_VERSION)
+        self.site_service = self.client.GetService("SiteService", version=API_VERSION)
+        self.company_service = self.client.GetService("CompanyService", version=API_VERSION)
+        self.report_downloader = self.client.GetDataDownloader(version=API_VERSION)
 
     def set_admanager_client(self, network_code: str=None) -> AdManagerClient:
         credentials = Database().get_credentials(self.email)
@@ -32,7 +31,7 @@ class AdOpsAdManagerClient:
             return AdManagerClient(refresh_token_client, credentials.app_name, network_code)
         else:
             client = AdManagerClient(refresh_token_client, credentials.app_name)
-            all_networks = client.GetService("NetworkService", version=self._API_VERSION).getAllNetworks()
+            all_networks = client.GetService("NetworkService", version=API_VERSION).getAllNetworks()
             print("Available Ad manager networks:")
             network_codes = {}
             for index, network in enumerate(all_networks):
@@ -41,3 +40,37 @@ class AdOpsAdManagerClient:
             if not network_code:
                 network_code = network_codes.get(input("Pick number: "))
                 return AdManagerClient(refresh_token_client, credentials.app_name, network_code)
+
+    def build_statement(self, key, value, limit=500, contains=False):
+        statement = (
+            StatementBuilder(version=API_VERSION)
+            .OrderBy(f"{key}", ascending=True)
+            .Limit(limit)
+        )
+        if isinstance(key, list):
+            statement = (statement
+            .Where(f"{key} IN (:{key})")
+            .WithBindVariable(f"{key}", ",".join(value))
+            )
+        elif contains:
+            statement = statement.Where(f"{key} LIKE '%{value}%'")
+        else:
+            statement = (statement
+                .Where(f"{key} = :{key}")
+                .WithBindVariable(f"{key}", value)
+            )
+
+        return statement
+    
+    def get_items_by_statement(self, statement: StatementBuilder, callback):
+        items = []
+        while True:
+            response = callback(statement.ToStatement())
+            logger.info(f"startIndex: {response['startIndex']}, totalResultSetSize: {response['totalResultSetSize']}")
+            if "results" in response and response["results"]:
+                items.extend(response["results"])
+                statement.offset += statement.limit
+            else:
+                break
+
+        return items
