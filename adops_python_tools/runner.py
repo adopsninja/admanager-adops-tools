606500d026b5f2cb23ffc489990a0745350f6433
adopsninja <siudak.dariusz@gmail.com>
2022-03-29 14:49:22 +0200

    Add option in runner to run MOX mcm update manually.

--- adops_python_tools/runner.py
+++ adops_python_tools/runner.py
@@ -2,6 +2,7 @@
 import logging
 
 from database import Database
+from mcm_manager import mox_mcm_status_update
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logging.getLogger("googleapiclient.discovery_cache").setLevel(logging.ERROR)
@@ -10,7 +11,8 @@ logger = logging.getLogger(__name__)
 def main():
     print("\n:::AdOps Python Tools:::")
     options = {
-        "1": "Database module"
+        "1": "Database module",
+        "2": "Company M mcm status"
     }
     while True:
         print("\nWhat module would you like to use next? (press q to quit)")
@@ -18,6 +20,8 @@ def main():
         choice = input("Pick a number: ")
         if choice == "1":
             Database().database_CLI()
+        elif choice == "2":
+            mox_mcm_status_update("mox")
         else:
             break
 
